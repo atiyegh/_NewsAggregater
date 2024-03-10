47af8a75287f9f2cb8cf03f0c0a751b665338b4e
Atiye <atiye.ghorbani@gmail.com>
2024-03-09 23:36:37 -0800

    Added: getting filter values & making queries for the guardian api

--- src/pages/homePage/index.jsx
+++ src/pages/homePage/index.jsx
@@ -1,40 +1,72 @@
 import React, {useEffect, useState} from 'react';
-import {Col, Row, Form, Select, Input, Button, DatePicker} from "antd";
+import {Col, Row, Form, Select, Input, Button, DatePicker, Typography} from "antd";
 import {categories, sources} from "../../data";
 import {SearchOutlined} from "@ant-design/icons";
 import {getNewsFromGuardian, getNewsFromNewsApi, getNewsFromNYTimes} from "../../Api";
 const { RangePicker } = DatePicker;
 function HomePage() {
     const [form]=Form.useForm();
-    const [loading, setLoading] = useState(false);
+    // const [loading, setLoading] = useState(false);
 
 
     const handleFormSubmit=(values)=>{
         console.log("values", values)
+        getNews(values)
     }
 
     useEffect(() => {
         getNews()
     }, []);
 
-    async function getNews(){
-        setLoading(true)
-        try{
-            const getNewsGuardianResponse=await getNewsFromGuardian()
-            console.log({getNewsGuardianResponse})
-            if(getNewsGuardianResponse?.response?.status==="ok"){
-                const getNewsApiResponse=await getNewsFromNewsApi();
-                console.log({getNewsApiResponse})
-                if(getNewsApiResponse?.status==="ok"){
-                    const getNewsNYTimesResponse=await getNewsFromNYTimes()
-                    console.log({getNewsNYTimesResponse})
-                }
-            }
-        }catch(error){
-            console.log({error})
-        }finally {
+    function makeQueriesForGuardian(filterValues){
+        const {category,keyword,source, fromPublishedDate,toPublishedDate}=filterValues;
+        console.log({source})
+        const categoryQuery=category?`sectionName=${category}`:'';
+        const keywordQuery=keyword?`webTitle=${keyword}`:'';
+        const fromPublishedDateQuery=fromPublishedDate?`from-date=${fromPublishedDate?.toISOString()}`:''
+        const toPublishedDateQuery=toPublishedDate?`to-date=${toPublishedDate?.toISOString()}`:''
 
-        }
+        console.log({categoryQuery},{keywordQuery},{fromPublishedDateQuery},{toPublishedDateQuery} )
+    }
+
+    function getFilterValues(){
+        const category=form.getFieldValue("category") || undefined;
+        const keyword=form.getFieldValue("keyword") || undefined;
+        const source=form.getFieldValue("source") || undefined;
+        const fromPublishedDate=(form.getFieldValue("publishDate") &&
+            (form.getFieldValue("publishDate")[0]<=form.getFieldValue("publishDate")[1]?form.getFieldValue("publishDate")[0]:form.getFieldValue("publishDate")[1]
+            )) || undefined
+        const toPublishedDate=(form.getFieldValue("publishDate") &&
+            (form.getFieldValue("publishDate")[0]>form.getFieldValue("publishDate")[1]?form.getFieldValue("publishDate")[0]:form.getFieldValue("publishDate")[1]
+            )) || undefined
+
+        return {category,keyword,source,fromPublishedDate,toPublishedDate}
+    }
+    async function getNews(filters){
+        // setLoading(true)
+        const filterValues=filters && getFilterValues()
+        filterValues && filterValues.source==="theguardian" && makeQueriesForGuardian(filterValues)
+
+        const queriesForGuardian=filters ? `` :"";
+        const queriesForNewsApi=filters ? `` : "";
+        const queriesForNYTimes=filters? `` :"";
+
+        // try{
+        //     const getNewsGuardianResponse=await getNewsFromGuardian(queriesForGuardian ?? "")
+        //     console.log({getNewsGuardianResponse})
+        //     if(getNewsGuardianResponse?.response?.status==="ok"){
+        //         const getNewsApiResponse=await getNewsFromNewsApi(queriesForNewsApi ?? "");
+        //         console.log({getNewsApiResponse})
+        //         if(getNewsApiResponse?.status==="ok"){
+        //             const getNewsNYTimesResponse=await getNewsFromNYTimes(queriesForNYTimes ?? "")
+        //             console.log({getNewsNYTimesResponse})
+        //         }
+        //     }
+        // }catch(error){
+        //     console.log({error})
+        // }finally {
+        //     // setLoading(false)
+        // }
     }
 
     return (
@@ -45,7 +77,21 @@ function HomePage() {
                 onFinish={handleFormSubmit}
                 style={{width:'100%'}}
                >
-                   <Row wrap={true} gutter={[25]}>
+                   <Row justify={"center"} style={{marginTop:'35px'}}>
+                       <Col span={10}>
+                           <Row style={{width:'100%'}}>
+                               <Typography style={{fontSize:'12px'}}>Search for News</Typography>
+                           </Row>
+                           <Row>
+                               <Col span={24}>
+                                   <Form.Item name={"keyword"}  >
+                                       <Input placeholder={'Enter a keyword...'} addonBefore={<SearchOutlined />}/>
+                                   </Form.Item>
+                               </Col>
+                           </Row>
+                       </Col>
+                   </Row>
+                   <Row wrap={true} gutter={[25]} justify={"center"}>
                        <Col span={4}>
                            <Form.Item name={"source"} label={"Sources"} >
                                <Select
@@ -74,11 +120,11 @@ function HomePage() {
                            <Button
                                type={"primary"}
                                size={"middle"}
-                               loading={loading}
+                               // loading={loading}
                                htmlType={"submit"}
-                               icon={<SearchOutlined />}
+                               // icon={<SearchOutlined />}
                                color={"#E74E22"}
-                               style={{background:"E74E22" , color:'white', width:'185px'}}
+                               style={{ width:'100px'}}
                            >
                                Search
                            </Button>
